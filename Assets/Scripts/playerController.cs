df705a9955aa0e170c5995b55c1f71393ba7c462
flint7577 <flint7577@gmail.com>
2025-11-23 23:05:11 -0500

    Add player animation assets and controller
    Added new animation clips (Idle, Run, Jump, Fire, Hurt, JumpFire) and their meta files for the player character. Introduced PlayerAnim.controller to manage animation states and transitions. Updated Game.unity and playerController.cs to integrate the new animations. (brain hurt so mush i want to swim in hot warm water with my belly out)

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -12,7 +12,7 @@ public class playerController : MonoBehaviour
 
     private LayerMask groundLayer;
 
-
+    private Animator anim;
     
     public bool isGrounded = false;
     public float groundCheckRadius = 0.02f;
@@ -26,19 +26,45 @@ public class playerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         groundLayer = LayerMask.GetMask("Ground");
+
+        anim = GetComponent<Animator>();
+
+        //initialize ground check POOsition
+        GameObject newObj = new GameObject("GroundCheck");
+        newObj.transform.SetParent(transform);
+        newObj.transform.localPosition = Vector3.zero;
+        groundCheck = newObj.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        float hValue = Input.GetAxis("Horizontal"); //left & right movement
 
+        //left & right movement
+        float hValue = Input.GetAxis("Horizontal"); 
         rb.linearVelocityX = hValue * speed;
 
-        if (Input.GetButtonDown("Jump")) //jump functionality
+
+        //jump functionality
+        if (Input.GetButtonDown("Jump")) 
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+
+            anim.SetBool("isJumping", !isGrounded); //jump animation
         }
+
+        //attack animation using "Fire1"
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("Fire");
+        }
+
+        //fire key + jump key = jump attack animation
+        if (Input.GetButton("Fire1") && Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("JumpFire");
+        }
+
     }
 }
